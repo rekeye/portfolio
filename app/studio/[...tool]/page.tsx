169cd5a7651717dbd07ef957b93e4b339c018b0f
Szymon Paluch <12szymon.paluch34@gmail.com>
2026-05-31 13:09:45 +0200

    feat: hide studio on prod

--- app/studio/[...tool]/page.tsx
+++ app/studio/[...tool]/page.tsx
@@ -4,7 +4,12 @@ import config from "../../../sanity.config";
 export const dynamic = "force-static";
 
 export { metadata, viewport } from "next-sanity/studio";
+import { notFound } from "next/navigation";
 
 export default function StudioPage() {
+  if (process.env.NODE_ENV === "production") {
+    notFound();
+  }
+
   return <NextStudio config={config} />;
 }
